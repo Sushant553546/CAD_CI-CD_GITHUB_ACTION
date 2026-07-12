1662788fa8c7e4db81d4fadc59f07dfb1109c339
Sushant553546 <raut.sushant2005@gmail.com>
2026-07-12 23:41:13 +0545

    mdified

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def save_books(books):
 
 # --- API Endpoints ---
 @app.get("/") 
-def home(): return { "message":"CI/CD Deployment Successful!" }
+def home(): return { "message":"CI/CD Deployment Successful!!" }
 
 @app.get("/", tags=["Root"])
 def read_root():
